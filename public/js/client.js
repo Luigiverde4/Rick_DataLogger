26d979558647599c2a323a728a3a963e31b0e54b
R.Roman <luigiverdeyt@gmail.com>
2025-01-03 06:48:27 +0100

    Grafico como clase
    Ahora los graficos son su propia clase para poder meterles metodos y atributos comunes entre ellos para tener varios a la vez y poder returilizar codigo

--- public/js/client.js
+++ public/js/client.js
@@ -7,11 +7,24 @@ cliente
 
 const sock = io()
 elementoEstado = document.getElementById("estadoServer")
+graficos = []
 
 sock.on("connect",(msj)=>{
     console.log("Conectado al servidor")
     elementoEstado.textContent = "Estado de la conexion: Conectado"
     console.log("Mensaje recibido\n",msj)
+
+    // Generar grafico
+    // Crear instancias de la clase Grafico para cada cliente
+    grafico1 = new Grafico("myChart", sock.id);
+    graficos.push(grafico1)
+    setInterval(() => {
+        // Generar nueva medición
+        let nuevaMedicion = parseInt(Math.random() * 100);
+        // Actualizar los gráficos de los clientes
+        grafico1.actualizarDatos(nuevaMedicion);
+    }, 1000); // Intervalo de 500ms para obtener muestras rápidamente
+    
 });
 
 sock.on("disconnect",()=>{
@@ -19,87 +32,6 @@ sock.on("disconnect",()=>{
     elementoEstado.textContent = "Estado de la conexion: Desconectado"
 });
 
-
-// Graficos - https://www.youtube.com/watch?v=_vRS87AT1Yk
-let ctx = document.getElementById("myChart").getContext("2d");
-
-mediciones = []
-
-datos_graficos = {
-    labels: [],
-    datasets: [{
-        label: "Valores",
-        data: [],
-        backgroundColor: [
-            "rgb(202, 64, 29)",
-        ]
-    }]
-}
-
-opciones_graficos = {
-    scales: {
-        y: {
-            beginAtZero: true,
-        },
-    }
-}
-
-let myChart = new Chart(ctx, {
-    type: "line",
-    data: datos_graficos,
-    options: opciones_graficos
-});
-
-
-
-
-let cantMuestrasParaActualizar = 3; // Cantidad de muestras que se van a actualizar de golpe
-let contadorAcumulacion = 0; // Contador de cuantas iteraciones llevamos antes de actualizar
-const maxLength = 10; // cantidad maxima de puntos representados en el grafico
-
-setInterval(() => {
-    // Generar nueva medicion
-    let nuevaMedicion = parseInt(Math.random() * 100);
-
-    // Anadir la nueva medicion a la lista
-    if (mediciones.length < maxLength) {
-        mediciones.push(nuevaMedicion);
-    } else {
-        mediciones.shift(); // Sacar el mas viejo
-        mediciones.push(nuevaMedicion); // Anadir el nuevo
-    }
-
-    // Actualizar eje X
-    datos_graficos.labels.push(cogerHoraMinutoSegundos());
-
-    // Actualizar eje Y
-    datos_graficos.datasets[0].data.push(nuevaMedicion);
-
-    // Limitar la longitud de los datos
-    if (datos_graficos.labels.length > maxLength) {
-        datos_graficos.labels.shift();
-        datos_graficos.datasets[0].data.shift();
-    }
-
-    // Aumentar el contador de muestras
-    contadorAcumulacion++;
-
-    // Cuando tengamos 3 muestras acumuladas, actualizamos el grafico
-    if (contadorAcumulacion >= cantMuestrasParaActualizar) {
-        myChart.update(); // Actualizar el grafico
-        contadorAcumulacion = 0; // Reiniciar el contador
-    }
-}, 500); // Intervalo de 500ms para obtener muestras rapidamente
-
-
-
-function cogerHoraMinutoSegundos(){
-    date = new Date()
-    let hora = date.getHours(); 
-    let minutos = date.getMinutes(); 
-    let segundos = date.getSeconds(); 
-    return `${hora}:${minutos}:${segundos}`;
-}
 
 function construyeVisualizadores(){
     console.log("construyeVisualizadores")
---
+++ public/js/grafico.js
@@ -0,0 +1,82 @@
+class Grafico {
+    // Constructor de clase
+    constructor(canvasId,sock_id) {
+        // Crear valores basicos del grafico
+        this.ctx = document.getElementById(canvasId).getContext("2d");
+        this.mediciones = []; // Mediciones que tendra 
+        this.maxLength = 10; // cantidad maxima de puntos representados en el grafico
+
+
+        this.cantMuestrasParaActualizar = 3;
+        this.contadorAcumulacion = 0;
+
+        this.id_cliente = sock_id
+        // Datos y opciones del gráfico
+        this.datos_graficos = {
+            labels: [],
+            datasets: [{
+                label: "Valores",
+                data: [],
+                backgroundColor: ["rgb(202, 64, 29)"],
+            }]
+        };
+
+        this.opciones_graficos = {
+            scales: {
+                y: { beginAtZero: true },
+            }
+        };
+
+        // Creamos la instancia de Chart()
+        this.myChart = new Chart(this.ctx, {
+            type: "line",
+            data: this.datos_graficos,
+            options: this.opciones_graficos
+        });
+    }
+
+    actualizarDatos(nuevaMedicion){
+        console.log(this.mediciones)
+        // Anadir la nueva medicion a la lista
+        if (this.mediciones.length < this.maxLength) {
+            this.mediciones.push(nuevaMedicion);
+        } else {
+            this.mediciones.shift(); // Sacar el mas viejo
+            this.mediciones.push(nuevaMedicion); // Anadir el nuevo
+        }
+
+        // Actualizar eje X
+        this.datos_graficos.labels.push(this.cogerHoraMinutoSegundos());
+
+        // Actualizar eje Y
+        this.datos_graficos.datasets[0].data.push(nuevaMedicion);
+
+        // Limitar la longitud de los datos
+        if (this.datos_graficos.labels.length > this.maxLength) {
+            this.datos_graficos.labels.shift();
+            this.datos_graficos.datasets[0].data.shift();
+        }
+
+        // Aumentar el contador de muestras
+        this.contadorAcumulacion++;
+
+        // Cuando tengamos 3 muestras acumuladas, actualizamos el grafico
+        if (this.contadorAcumulacion >= this.cantMuestrasParaActualizar) {
+            this.myChart.update(); // Actualizar el grafico
+            this.contadorAcumulacion = 0; // Reiniciar el contador
+        }
+    }
+
+    // Sacar hora minuto segundos para eje X
+    cogerHoraMinutoSegundos() {
+        let date = new Date();
+        let hora = date.getHours();
+        let minutos = date.getMinutes();
+        let segundos = date.getSeconds();
+        return `${hora}:${minutos}:${segundos}`;
+    }
+}
+    
+
+
+
