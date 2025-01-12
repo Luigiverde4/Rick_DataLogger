6857f9dd79721bd15781c2a9f3aaa0585eaf9613
R.Roman <luigiverdeyt@gmail.com>
2025-01-12 22:43:35 +0100

    Valores medios, minimos y maximos representados por pantalla

--- public/js/client.js
+++ public/js/client.js
@@ -6,14 +6,33 @@ cliente
 */
 
 const sock = io();
+
+// DOM - info de conexiones
 elementoEstado = document.getElementById("estadoServer");
 elementoCantEspect = document.getElementById("cantEspect");
 elementoCantSensor = document.getElementById("cantSensores");
-graficos = [];
 
-// Referencias para la bola y el plano cartesiano
-ball = document.getElementById('ball');
-plane = document.getElementById('cartesian-plane');
+// DOM - valores calculados
+valorMedioX = document.getElementById("valor_medio_x");
+valorMaximoX = document.getElementById("valor_maximo_x");
+valorMinimoX = document.getElementById("valor_minimo_x");
+
+valorMedioY = document.getElementById("valor_medio_y");
+valorMaximoY = document.getElementById("valor_maximo_y");
+valorMinimoY = document.getElementById("valor_minimo_y");
+
+// Graficos y sus datos
+graficos = [];
+let valoresX = [];
+let valoresY = [];
+let maxX = -Infinity;
+let minX = Infinity;
+let maxY = -Infinity;
+let minY = Infinity;
+
+// DOM - representacion bola M5
+bola = document.getElementById('bola');
+plane = document.getElementById('plano-cartesiano');
 
 // Calcular el centro del plano cartesiano (0, 0)
 const planeRect = plane.getBoundingClientRect();
@@ -33,9 +52,7 @@ sock.on("connect", (msj) => {
 
 
     sock.on("valoresIniciales", ({ valores_x, valores_y }) => {
-        console.log("Valores iniciales recibidos:", valores_y);
-
-        // Actualizar datos de los gráficos
+        // Actualizar datos de los graficos
         for (let valor of valores_x) {
             g_acelX.actualizarDatos(valor);
         }
@@ -54,18 +71,25 @@ sock.on("disconnect", () => {
 
 // Recibir datos del servidor
 sock.on("datosIMU", (datos) => {
-    // Actualizar los gráficos con los datos de aceleración
-    g_acelX.actualizarDatos(datos.aceleracionX);
-    g_acelY.actualizarDatos(datos.aceleracionY);
+    // Cogemos los valores de aceleracion
+    acelX = datos.aceleracionX
+    acelY = datos.aceleracionY
+
+    // Actualizar los graficos con los datos de aceleración
+    g_acelX.actualizarDatos(acelX);
+    g_acelY.actualizarDatos(acelY);
 
     // Obtener las coordenadas y límites de la bola
-    const px = datos.posicionX;
-    const py = datos.posicionY;
-    const limiteX = datos.limiteX;
-    const limiteY = datos.limiteY;
+    px = datos.posicionX;
+    py = datos.posicionY;
+    limiteX = datos.limiteX;
+    limiteY = datos.limiteY;
 
     // Actualizar la bola con los datos recibidos
     actualizarBola(px, py, limiteX, limiteY);
+
+    // Actualizar mediidas de las graficas
+    actualizarValoresCalculados(acelX,acelY)
 });
 
 // Otros datos de conexión
@@ -86,15 +110,45 @@ function actualizarBola(x, y, limiteX, limiteY) {
     y = Math.max(0, Math.min(240, y)); // Limitar Y entre 0 y 240
 
     // Calcular la posición de la bola en el plano
-    const ballX = centerX + (x - 160) - ball.offsetWidth / 2; // Ajustar al rango [0, 320]
-    const ballY = centerY + (y - 120) - ball.offsetHeight / 2; // Ajustar al rango [0, 240]
+    bolaX = centerX + (x - 160) - bola.offsetWidth / 2; // Ajustar al rango [0, 320]
+    bolaY = centerY + (y - 120) - bola.offsetHeight / 2; // Ajustar al rango [0, 240]
 
     // Colocar la bola en su nueva posición
-    ball.style.left = `${ballX}px`;
-    ball.style.top = `${ballY}px`;
+    bola.style.left = `${bolaX}px`;
+    bola.style.top = `${bolaY}px`;
 
     // Cambiar el color de la bola si se encuentra en los límites
     let color = limiteX || limiteY ? "blue" : "red";
-    ball.style.backgroundColor = color;
+    bola.style.backgroundColor = color;
 }
 
+function actualizarValoresCalculados(acelX,acelY){
+    // Convertir los valores a números
+    acelX = parseFloat(acelX);
+    acelY = parseFloat(acelY);
+
+    // Actualizar max y mins de X
+    maxX = acelX > maxX ? acelX : maxX;
+    minX = acelX < minX ? acelX : minX;
+    // Actualizar max y mins de Y
+    maxY = acelY > maxY ? acelY : maxY;
+    minY = acelY < minY ? acelY : minY;
+
+    // Guardamos los valores
+    valoresX.push(acelX);
+    valoresY.push(acelY);
+
+    // Promedios
+    promedioX = valoresX.reduce((a, b) => a + b, 0) / valoresX.length // np.sum(x)/len(x)
+    promedioY = valoresY.reduce((a, b) => a + b, 0) / valoresY.length
+    
+
+    // Actualizar el DOM
+    valorMedioX.textContent = promedioX.toFixed(2);
+    valorMaximoX.textContent = maxX.toFixed(2);
+    valorMinimoX.textContent = minX.toFixed(2);
+
+    valorMedioY.textContent = promedioY.toFixed(2);
+    valorMaximoY.textContent = maxY.toFixed(2);
+    valorMinimoY.textContent = minY.toFixed(2);
+}
